51a05963c9c2bdaffd50b14a724f3c5d5c745d2d
Angel <AngelVanegas944@gmail.com>
2025-05-27 13:11:36 -0500

    ACTUALIZACION DE HREF

--- views/home.php
+++ views/home.php
@@ -14,7 +14,7 @@
 <body>
     <header>
     <div class="logo">
-        <img src="/FINAL-LIBROS-WAP/public/img/logow.png" alt=""> LIBROS WAP
+        <img src="/WAPBOOKS/public/img/logow.png" alt=""> LIBROS WAP
     </div>
     <nav>
         <a href="#home">Home</a>
@@ -34,7 +34,7 @@
     <!-- home de imagen y primera vista -->
 
     <section id="home" class="imagen">
-        <img src="/FINAL-LIBROS-WAP/public/img/5.jpg" alt="Biblioteca">
+        <img src="/WAPBOOKS/public/img/5.jpg" alt="Biblioteca">
         <div class="texto-superpuesto">En Libros WAP creemos en el poder de los libros para transformar mentes, abrir
             mundos y conectar personas. Nuestra plataforma está diseñada para que amantes de la lectura puedan comprar,
             vender e intercambiar libros de manera fácil, segura y accesible.</div>
@@ -43,12 +43,12 @@
     <!-- SECCION DE PRODUCTOS -->
     <section id="products" class="products-section">
     <div class="product-block">
-        <img src="/FINAL-LIBROS-WAP/public/img/Nuevos.jpg" alt="Primera Mano">
+        <img src="/WAPBOOKS /public/img/Nuevos.jpg" alt="Primera Mano">
         <h2>Libros de Primera Mano</h2>
         <p>Encuentra libros totalmente nuevos, en perfecto estado y directamente de editoriales o distribuidores.</p>
     </div>
     <div class="product-block">
-        <img src="/FINAL-LIBROS-WAP/public/img/Segunda.jpg" alt="Segunda Mano">
+        <img src="/WAPBOOKS/public/img/Segunda.jpg" alt="Segunda Mano">
         <h2>Libros de Segunda Mano</h2>
         <p>Explora una amplia variedad de libros usados en buen estado a precios accesibles.</p>
     </div>
@@ -61,14 +61,14 @@
     <section id="categories" class="categories">
         <h2>Categorías</h2>
         <div class="category-list">
-            <div class="category-item">Ciencia Ficción<br><img src="/FINAL-LIBROS-WAP/public/img/ficcion.jpg" alt="">
+            <div class="category-item">Ciencia Ficción<br><img src="/WAPBOOKS/public/img/ficcion.jpg" alt="">
             </div>
-            <div class="category-item">Frases<br><img src="/FINAL-LIBROS-WAP/public/img/frases.jpg" alt=""></div>
-            <div class="category-item">Teatro<br><img src="/FINAL-LIBROS-WAP/public/img/teatro.jpg" alt=""></div>
-            <div class="category-item">Sátira<br><img src="/FINAL-LIBROS-WAP/public/img/satira.jpg" alt=""></div>
-            <div class="category-item">Autobiografía<br><img src="/FINAL-LIBROS-WAP/public/img/auto.jpg" alt=""></div>
-            <div class="category-item">Comedia<br><img src="/FINAL-LIBROS-WAP/public/img/comedia.jpg" alt=""></div>
-            <div class="category-item">Literatura Fantástica<br><img src="/FINAL-LIBROS-WAP/public/img/fantastico.jpg"
+            <div class="category-item">Frases<br><img src="/WAPBOOKS/public/img/frases.jpg" alt=""></div>
+            <div class="category-item">Teatro<br><img src="/WAPBOOKS/public/img/teatro.jpg" alt=""></div>
+            <div class="category-item">Sátira<br><img src="/WAPBOOKS/public/img/satira.jpg" alt=""></div>
+            <div class="category-item">Autobiografía<br><img src="/WAPBOOKS/public/img/auto.jpg" alt=""></div>
+            <div class="category-item">Comedia<br><img src="/WAPBOOKS/public/img/comedia.jpg" alt=""></div>
+            <div class="category-item">Literatura Fantástica<br><img src="/WAPBOOKS/public/img/fantastico.jpg"
                     alt=""></div>
         </div>
         </div>
@@ -80,19 +80,19 @@
 
         <div class="services-container">
             <div class="service-card">
-                <img src="/FINAL-LIBROS-WAP/public/img/compra.jpg" alt="Compra">
+                <img src="/WAPBOOKS/public/img/compra.jpg" alt="Compra">
                 <p class="service-text">COMPRA</p>
                 <a href="#">Ir a comprar</a>
             </div>
 
             <div class="service-card">
-                <img src="/FINAL-LIBROS-WAP/public/img/vende.jpg" alt="Vende">
+                <img src="/WAPBOOKS/public/img/vende.jpg" alt="Vende">
                 <p class="service-text">VENDE</p>
                 <a href="#">Ir a vender</a>
             </div>
 
             <div class="service-card selected">
-                <img src="/FINAL-LIBROS-WAP/public/img/Intercambia.jpg" alt="Intercambia">
+                <img src="/WAPBOOKS/public/img/Intercambia.jpg" alt="Intercambia">
                 <p class="service-text">INTERCAMBIA</p>
                 <a href="#">Ir a intercambiar</a>
             </div>
@@ -110,27 +110,27 @@
         <h2>Libros Destacados</h2>
         <div class="book-list">
             <div class="book-item">
-                <img src="/FINAL-LIBROS-WAP/public/img/poca.jpg" alt="Libro 1">
+                <img src="/WAPBOOKS/public/img/poca.jpg" alt="Libro 1">
                 <p>Tan poca vida</p>
                 <p>Hanya Yanagihara</p>
             </div>
             <div class="book-item">
-                <img src="/FINAL-LIBROS-WAP/public/img/camus.jpg" alt="Libro 2">
+                <img src="/WAPBOOKS/public/img/camus.jpg" alt="Libro 2">
                 <p>La peste</p>
                 <p>Albert Camus</p>
             </div>
             <div class="book-item">
-                <img src="/FINAL-LIBROS-WAP/public/img/cien.jpg" alt="Libro 3">
+                <img src="/WAPBOOKS/public/img/cien.jpg" alt="Libro 3">
                 <p>Cien años de soledad</p>
                 <p>Gabriel Garcia Marquez</p>
             </div>
             <div class="book-item">
-                <img src="/FINAL-LIBROS-WAP/public/img/loco.jpg" alt="Libro 4">
+                <img src="/WAPBOOKS/public/img/loco.jpg" alt="Libro 4">
                 <p>La historia del loco</p>
                 <p>John Katzenbach</p>
             </div>
             <div class="book-item">
-                <img src="/FINAL-LIBROS-WAP/public/img/meters.jpg" alt="Libro 5">
+                <img src="/WAPBOOKS/public/img/meters.jpg" alt="Libro 5">
                 <p>A dos metros de ti</p>
                 <p>Rachael Lippincot</p>
             </div>
@@ -143,7 +143,7 @@
     <section id="contact" class="contact">
         <h2>Contáctenos</h2>
         <div class="contact-imgs">
-            <img src="/FINAL-LIBROS-WAP/public/img/1.jpg" alt="Ilustración 1">
+            <img src="/WAPBOOKS/public/img/1.jpg" alt="Ilustración 1">
         </div>
         <div class="contact-desc">
             <p>Diseña conceptos para tu marca. Intercambia ideas con tu equipo y desarrolla el aspecto visual de tu
